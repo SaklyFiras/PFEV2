cc85d5cc1ab8f891d45765f249c9878f7316e222
SaklyFiras <79334170+SaklyFiras@users.noreply.github.com>
2023-04-23 00:02:25 +0100

    HI

--- frontend/src/constants/globalConstants.js
+++ frontend/src/constants/globalConstants.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:3000/api/v2";
-// export const BACKEND_URL = "https://dentistuptun.onrender.com";
+// export const BACKEND_URL = "http://localhost:3000/api/v2";
+export const BACKEND_URL = "https://dentistuptun.onrender.com";
