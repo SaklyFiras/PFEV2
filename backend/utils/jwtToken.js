577e1293b4bd947e7d329c82560fc4336b75df29
SaklyFiras <79334170+SaklyFiras@users.noreply.github.com>
2023-04-23 21:12:08 +0100

    hy

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -13,7 +13,7 @@ const sendToken = (user, statusCode, res) => {
 		SameSite: "None",
 		Secure: true,
 		Path: "/",
-		domain: process.env.ONLINE_URL,
+		domain: "dentistuptun.onrender.com",
 	};
 
 	res.status(statusCode).cookie("token", token, options).json({
