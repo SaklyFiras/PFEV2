7e1831fbd9028f89d1a06a35b2b78761610387d8
SaklyFiras <79334170+SaklyFiras@users.noreply.github.com>
2023-04-23 20:53:56 +0100

    hy

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,10 +5,17 @@ const sendToken = (user, statusCode, res) => {
 	//create jwt token
 	const token = user.getJwtToken();
 
-	
-	
+	const options = {
+		expires: new Date(
+			Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+		),
+		httpOnly: true,
+		SameSite:"None",
+		Secure: true,
+		Path: "/",
+	};
 
-	res.status(statusCode).cookie("token", token).json({
+	res.status(statusCode).cookie("token", token, options).json({
 		success: true,
 		token,
 		user,
--- frontend/src/constants/globalConstants.js
+++
@@ -1,2 +0,0 @@
-export const BACKEND_URL = "https://dentistuptun.onrender.com/api/v2";
-export const FRONTEND_URL = "https://dentistuptun.onrender.com"
