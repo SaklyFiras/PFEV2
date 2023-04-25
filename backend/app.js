ba9b6d77c63f39c7c0af9068f7af6b506053fd29
SaklyFiras <79334170+SaklyFiras@users.noreply.github.com>
2023-04-25 18:05:20 +0100

    hh

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,8 @@ const fileUpload = require("express-fileupload");
 const path = require("path");
 
 const errorMiddleware = require("./middlewares/errors");
-if (process.env.NODE_ENV !== "PRODUCTION") require("dotenv").config({ path: "backend/config/config.env" });
+if (process.env.NODE_ENV !== "PRODUCTION")
+	require("dotenv").config({ path: "backend/config/config.env" });
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -34,15 +35,15 @@ app.use("/api/v2", user);
 app.use("/api/v2", post);
 app.use("/api/v2", comment);
 
-if (process.env.NODE_ENV === 'PRODUCTION') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
-    })
-}
-
 //handling error
 app.use(errorMiddleware);
+
+if (process.env.NODE_ENV === "PRODUCTION") {
+	app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+	});
+}
 
 module.exports = app;
