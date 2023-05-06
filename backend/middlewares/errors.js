19d96ee3315a37a20dfa10293ce152061faa6c48
SaklyFiras <79334170+SaklyFiras@users.noreply.github.com>
2023-05-06 21:13:30 +0100

    AlhpaV1.09

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -2,7 +2,10 @@ const ErrorHandler = require("../utils/ErrorHandler");
 
 module.exports = (err, req, res, next) => {
 	err.statusCode = err.statusCode || 500;
-	if (process.env.NODE_env === "DEVELOPMENT") {
+
+	if (process.env.NODE_ENV === "DEVELOPMENT") {
+		console.log(err);
+
 		res.status(err.statusCode).json({
 			success: false,
 			error: err,
@@ -10,40 +13,45 @@ module.exports = (err, req, res, next) => {
 			stack: err.stack,
 		});
 	}
-	if (process.env.NODE_env === "PRODUCTION") {
+
+	if (process.env.NODE_ENV === "PRODUCTION") {
 		let error = { ...err };
-		error.message = err.message || "server error";
-		//wrong mongoose Object ID error
-		if (err.name == "CastError") {
-			const message = `Resource not found .Invalid : `;
+
+		error.message = err.message;
+
+		// Wrong Mongoose Object ID Error
+		if (err.name === "CastError") {
+			const message = `Resource not found. Invalid: ${err.path}`;
 			error = new ErrorHandler(message, 400);
 		}
 
-		//handling mongoose validation error
-		if ((err.name = "validationError")) {
+		// Handling Mongoose Validation Error
+		if (err.name === "ValidationError") {
 			const message = Object.values(err.errors).map((value) => value.message);
 			error = new ErrorHandler(message, 400);
 		}
-		//Handling Mongoose duplicate key error
-		if (err.code === 11000) {
-			const message = `Duplicate ${Object.keys(err.keyValue)} entred`;
-			error = new ErrorHandler(message, 400);
-		}
-		//Handling wrong jwt error
-		if ((err.name = "jsonWebTokenError")) {
-			const message = "Json web token is invalid ,try again";
-			error = new ErrorHandler(message, 400);
-		}
-		//handling expired jwt error
-		if ((err.name = "TokenExpiredError")) {
-			const message = "Json Web Token is expired";
-			error = new ErrorHandler(message, 400);
-		}
-		
 
-		res.status(error.statusCode).json({
+		// Handling Mongoose duplicate key errors
+		if (err.code === 11000) {
+			const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+			error = new ErrorHandler(message, 400);
+		}
+
+		// Handling wrong JWT error
+		if (err.name === "JsonWebTokenError") {
+			const message = "JSON Web Token is invalid. Try Again!!!";
+			error = new ErrorHandler(message, 400);
+		}
+
+		// Handling Expired JWT error
+		if (err.name === "TokenExpiredError") {
+			const message = "JSON Web Token is expired. Try Again!!!";
+			error = new ErrorHandler(message, 400);
+		}
+
+		res.status(error.statusCode || 500).json({
 			success: false,
-			message: error.message || " server error",
+			message: error.message || "Internal Server Error",
 		});
 	}
 };
